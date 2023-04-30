1c0db1fb1bb2b390c9b260116884abc15d0061f8
konu96 <ukouhei96@gmail.com>
2023-04-30 14:35:43 +0900

    Fix app name

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -6,11 +6,11 @@ import { setupApp } from '~/logic/common-setup'
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
-  console.info('[vitesse-webext] Hello world from content script')
+  console.info('[side-notion-editor] Hello world from content script')
 
   // communication example: send previous tab title from background page
   onMessage('tab-prev', ({ data }) => {
-    console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
+    console.log(`[side-notion-editor] Navigate from page "${data.title}"`)
   })
 
   // mount component to context window
